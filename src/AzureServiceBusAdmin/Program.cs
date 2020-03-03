2485dea6682e4b12b4c5a9a4f821194b223dfe23
Chris Sheehan <chris_sheehan@hotmail.co.uk>
2020-03-03 13:53:48 +0000

    Initial commit

---
+++ src/AzureServiceBusAdmin/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AzureServiceBusAdmin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
